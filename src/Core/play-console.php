d86360c11fef456d0c6f9fc43150825a201b73ab
Maous-B <79797065+Maous-B@users.noreply.github.com>
2023-05-08 00:31:45 +0200

    Update play-console.php

--- src/Core/play-console.php
+++ src/Core/play-console.php
@@ -2,39 +2,89 @@
 
 require '../../vendor/autoload.php';
 
-echo "*** Création d'un jeu de 32 cartes.***\n";
-$cardGame = new App\Core\CardGame(App\Core\CardGame::factory32Cards());
+echo " ==== Quel nombre de cartes voulez-vous ? ==== \n ==== 1. Jeu de 32 cartes ==== \n ==== 2. Jeu de 52 cartes ==== \n";
+
+$choixJeu = readline();
+
+while($choixJeu > 2 or $choixJeu < 1)
+{
+    echo " Erreur de saisie. Veuillez recommencer : \n";
+    $choixJeu = readline();
+}
+
+if($choixJeu == 1)
+{
+    $cardGame = new App\Core\CardGame(App\Core\CardGame::factory32Cards());
+}
+else
+{
+    $cardGame = new App\Core\CardGame(App\Core\CardGame::factory52Cards());
+}
+
+echo " ==== Voulez-vous de l'aide ? ==== \n ==== 1. Oui ==== \n ==== 2. Non ==== \n";
+
+$choixAide = readline();
+
+while($choixAide > 2 or $choixAide < 1)
+{
+    echo " Erreur de saisie. Veuillez recommencer : \n";
+    $choixAide = readline();
+}
+
+if($choixAide == 1)
+{
+    $assistance = true;
+}
+else
+{
+    $assistance = false;
+}
 
 echo " ==== Instanciation du jeu, début de la partie. ==== \n";
-$game =  new App\Core\Guess($cardGame, $cardGame->getCard(0), false);
+echo " ==== Choix aléatoire de la carte ==== \n";
+$carteChoisieAuHasard = rand(0, count($cardGame->getCards()) - 1);
+$game =  new App\Core\Guess($cardGame, $carteChoisieAuHasard, $assistance);
+echo $cardGame->getCard($carteChoisieAuHasard);
 
-$userCardIndex = readline("Entrez une position de carte dans le jeu : ");
+echo " ==== Choix du nombre d'essais. ==== \n";
+
+echo "Combien d'essais ? :";
+$essais = readline();
+
 
 echo "=== Choix de carte === \n";
 
-$nomCarte = readline("Nom : ");
-$couleurCarte = readline("Couleur : ");
 
-$carteChoisie = new \App\Core\Card($nomCarte, $couleurCarte);
-echo $carteChoisie;
-
-while(1)
+while($essais > 0)
 {
+
+    if($essais = $essais / 2 and $game->getWithHelp() == true)
+    {
+        echo "L'aide a été déclenchée. Voici votre indice : ";
+        echo  $game->gameAssist();
+    }
+    echo "Nom : ";
+    $nomCarte = readline();
+    echo "Couleur : ";
+    $couleurCarte = readline();
+    $carteChoisie = new \App\Core\Card($nomCarte, $couleurCarte);
+
     if($game->isMatch($carteChoisie))
     {
-        echo " Bravo ! \n";
+        echo "Bravo ! La carte était belle et bien $carteChoisie\n";
         break;
     }
     else
     {
-        echo " Loupé !\n";
-        $nomCarte = readline("Nom : ");
-        $couleurCarte = readline("Couleur : ");
+        $essais--;
+        echo "Loupé ! Il vous reste $essais\n";
     }
 }
-// code naïf, car aucun contrôle de validité de $userCardIndex...
-$userCard = $cardGame->getCard($userCardIndex);
 
+if($essais == 0)
+{
+    echo "Perdu ! La carte était $carteChoisie";
+}
 
 echo " ==== Fin prématurée de la partie ====\n";
 echo "*** Terminé ***\n";
